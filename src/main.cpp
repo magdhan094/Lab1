c23657581c25a57cf399e5ea7020f1bd3fde34cd
Student <student@ib.polsl.pl>
2019-03-13 10:29:28 +0100

    Trzeci i czwarty commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#else
+#include <cmath>
+#endif
+
+
 int main(){
-return 0;
+#ifdef USE_TRIGONOMETRY_DEGREE
+std::cout<<degreemath::sin(30);
+#else
+std::cout<<std::sin(45.0);
+#endif
 }
